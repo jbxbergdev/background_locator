cb08baab70b1d626e766c0b3eeee13bf2f05e83a
Johannes Bolz <kontakt@johannes-bolz.de>
2021-09-30 13:01:54 +0200

    added some log outputs

--- lib/callback_dispatcher.dart
+++ lib/callback_dispatcher.dart
@@ -14,11 +14,13 @@ void callbackDispatcher() {
 
   _backgroundChannel.setMethodCallHandler((MethodCall call) async {
     if (Keys.BCM_SEND_LOCATION == call.method) {
+      print('##### BCM_SEND_LOCATION');
       final Map<dynamic, dynamic> args = call.arguments;
       final Function callback = PluginUtilities.getCallbackFromHandle(
           CallbackHandle.fromRawHandle(args[Keys.ARG_CALLBACK]))!;
       final LocationDto location =
           LocationDto.fromJson(args[Keys.ARG_LOCATION]);
+      print('##### calling callback');
       callback(location);
     } else if (Keys.BCM_NOTIFICATION_CLICK == call.method) {
       final Map<dynamic, dynamic> args = call.arguments;
